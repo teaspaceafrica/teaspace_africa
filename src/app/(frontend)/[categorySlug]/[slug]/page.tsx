f42d63228aaedf65e00679cc2b7228adfebe8dd7
geofrey <devongeoffreymaina@gmail.com>
2025-08-18 10:42:33 +0300

    Twitter thumbnail update

--- src/app/(frontend)/[categorySlug]/[slug]/page.tsx
+++ src/app/(frontend)/[categorySlug]/[slug]/page.tsx
@@ -104,18 +104,6 @@ export async function generateMetadata({
       shortcut: '/favicon.ico',
       apple: '/apple-touch-icon.png',
     },
-
-    other: {
-      'og:title': `${postTitle} | TeaSpace`,
-      'og:description': postExcerpt,
-      'og:image': imageUrl,
-      'og:url': pageUrl,
-      'og:type': 'article',
-      'twitter:image': imageUrl,
-      'twitter:title': `${postTitle} | TeaSpace`,
-      'twitter:description': postExcerpt,
-      'twitter:card': 'summary_large_image',
-    },
   }
 }
 
